ea683fe4f287b0b6331d37c4a87f131c39f5cf78
HAGENAJ <julian.hagenauer@daimler.com>
2021-06-07 12:21:22 +0200

    Removed example 2

--- example/gwann_example.R
+++ example/gwann_example.R
@@ -14,9 +14,9 @@ r<-gwann(x=x,y=y,dm=dm,
          adaptive=F,
          #bandwidth=10,
          bwSearch="goldenSection",
-         minBw=1, maxBw=3, steps=10,
+         bwMin=1, bwMax=3, steps=10,
          threads=8
-         )
+)
 print(paste("RMSE: ",r$rmse))
 print(paste("Iterations: ",r$its))
 print(paste("Bandwidth: ",r$bw))
@@ -30,57 +30,3 @@ s<-cbind( t(r$weights[[2]]), toy4[,c("lon","lat")] )
 colnames(s)<-c(paste("Neuron",1:5),"Bias Neuron","lon","lat")
 m<-melt(s,id.vars=c("lon","lat"))
 ggplot(m,aes(lon,lat,fill=value)) + geom_raster() + facet_wrap(~variable) + scale_fill_viridis() + coord_fixed()
-
-#################
-
-library(rJava)
-.jinit()
-.jaddClassPath("inst/java/commons-math3-3.6.1.jar")
-.jaddClassPath("inst/java/gt-api-19.1.jar")
-.jaddClassPath("inst/java/gt-data-19.1.jar")
-.jaddClassPath("inst/java/gt-main-19.1.jar")
-.jaddClassPath("inst/java/gwann-0.0.2-SNAPSHOT.jar")
-.jaddClassPath("inst/java/jblas-1.2.4.jar")
-.jaddClassPath("inst/java/jts-core-1.14.0.jar")
-.jaddClassPath("inst/java/jts-io-1.14.0.jar")
-.jaddClassPath("inst/java/log4j-1.2.17.jar")
-
-
-data(toy4)
-
-dm<-as.matrix( dist(toy4[,c("lon","lat")])  )
-x<-as.matrix(toy4[,c("x1","x2")])
-y<-as.numeric( toy4[,c("y")] )
-
-x<-x
-y<-y
-nrHidden<-5
-bandwidth<-(-1)
-opt="sgd"
-threads<-8
-batchSize<-50
-lr<-0.01
-linOut<-T
-kernel<-"gaussian"
-adaptive<-T
-iterations<-352
-folds<-10
-repeats<-1
-patience<-100
-
-.jmethods("supervised.nnet.gwann.GWANN_RInterface")
-
-r<-.jcall(obj="supervised.nnet.gwann.GWANN_RInterface",method="run",returnSig = "Lsupervised/nnet/gwann/ReturnObject;",
-          .jarray(x,dispatch=T), y, .jarray(dm,dispatch=T),
-          as.integer(1:nrow(dm)), as.integer(1:nrow(dm)),
-          nrHidden,batchSize,opt,lr,linOut,
-          kernel,
-          bandwidth,
-          adaptive,
-          "goldenSection",
-          1,
-          100,
-          10,
-          iterations,patience,
-          folds,repeats,
-          threads)
